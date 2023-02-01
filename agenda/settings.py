26b77c5567f6782b80aae6543fcbf74351b78dd9
pabloN <22187793+pablons2@users.noreply.github.com>
2023-02-01 12:04:57 -0300

    Add files via upload

--- agenda/settings.py
+++ agenda/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jbffizljwaqctnu##s!)eaw)c3ihyxz-mk*fbk#leo)$x%%pj6'
+SECRET_KEY = 'django-insecure-nwp9!s@yphn#y!9nl9^*zfekvwn)lmxwbyjer_y^=!*z4t8n%^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
